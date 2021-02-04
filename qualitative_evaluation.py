dd7c53d8cda4472bb81524ee84700857a82a726e
Thijs <thijs@305.nl>
2021-02-04 14:49:21 +0100

    Added CER and WER

--- qualitative_evaluation.py
+++ qualitative_evaluation.py
@@ -1,7 +1,8 @@
 import pytorch_lightning
 import torch
 from ctcdecode import CTCBeamDecoder
-from dataset import LibriSpeechDataset, StringProcessor
+from dataset import LibriSpeechDataset
+import Levenshtein as Lev
 
 
 class QualitativeEvaluation(pytorch_lightning.callbacks.Callback):
@@ -40,13 +41,13 @@ class QualitativeEvaluation(pytorch_lightning.callbacks.Callback):
                 output = self.string_processor.labels_to_str(arg_maxes)
                 greedy = self.string_processor.labels_to_str(collapse_ctc(arg_maxes, blank_id=self.string_processor.blank_id))
                 print("output:\t\t\"%s\"" % output)
-                print("greedy:\t\t\"%s\"" % greedy)
+                print("greedy:\t\t\"%s\" cer: %f wer: %f" % (greedy, calc_cer(greedy, ground_truth), calc_wer(greedy, ground_truth)))
 
                 beam_result, beam_scores, timesteps, out_seq_len = self.beam_search.decode(log_probabilities.transpose(0, 1))
                 for i in range(5):
                     result = beam_result[0][i][0:out_seq_len[0][i]].numpy().tolist()
                     string = self.string_processor.labels_to_str(result)
-                    print("beam %d:\t\t\"%s\"" % (i, string))
+                    print("beam %d:\t\t\"%s\"  cer: %f wer: %f" % (i, string, calc_cer(greedy, ground_truth), calc_wer(greedy, ground_truth)))
 
                 print('')
 
@@ -60,3 +61,23 @@ def collapse_ctc(arg_maxes, blank_id=27):
             continue
         result.append(index.item())
     return result
+
+
+def calc_wer(hyp, ref):
+    # build mapping of words to integers
+    b = set(hyp.split() + ref.split())
+    word2char = dict(zip(b, range(len(b))))
+
+    # map the words to a char array (Levenshtein packages only accepts strings)
+    w1 = [chr(word2char[w]) for w in hyp.split()]
+    w2 = [chr(word2char[w]) for w in ref.split()]
+
+    distance = Lev.distance(''.join(w1), ''.join(w2))
+    return distance / len(ref.split()) * 100
+
+
+def calc_cer(hyp, ref):
+    hyp, ref, = hyp.replace(' ', ''), ref.replace(' ', '')
+    distance = Lev.distance(hyp, ref)
+    return distance / len(ref) * 100
+
